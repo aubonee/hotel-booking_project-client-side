2ddb345b03a039c049f0cbcc98294d4cf994db7b
aubonee <chytahsin.2210@gmail.com>
2023-11-07 09:56:13 +0600

    login section added

---
+++ src/pages/login/GoogleLogin.jsx
@@ -0,0 +1,42 @@
+import  { useContext,  } from 'react';
+import { AuthContext } from '../../providers/AuthProvider';
+import { useLocation, useNavigate } from 'react-router-dom';
+const GoogleLogin = () => {
+
+     // const[user,setUser] =useState(null);
+     const {googleSignIn} =useContext(AuthContext);
+     const location = useLocation();
+     const navigate = useNavigate();
+     console.log(' login page', location);
+     
+    // const auth = getAuth(app);
+    // const provider = new GoogleAuthProvider();
+ 
+     const handleGoogleSignIn =()=>{
+         console.log('google sign in')
+       
+         googleSignIn()
+           .then((result) => {
+                 
+                 const loggedInUser = result.user;
+                 console.log(loggedInUser);
+                 console.log(result.user.displayName)
+                 console.log(loggedInUser.displayName)
+                  // navigate after login
+                  navigate(location?.state ? location.state : '/');
+                
+               
+               })
+               .catch((error) => {
+                 console.error(error);
+             });
+    
+      }
+    return (
+        <div>
+             <button onClick={handleGoogleSignIn } className='font-extrabold underline underline-offset-4  text-[#702632]'>Google Login</button>
+        </div>
+    );
+};
+
+export default GoogleLogin;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,49 @@
 import { Link } from "react-router-dom/dist";
-
+import { useContext, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
+import Swal from 'sweetalert2';
+import GoogleLogin from './GoogleLogin';
 
 const Login = () => {
+    const[user,setUser] =useState(null); 
+
+    const {signIn} =useContext(AuthContext);
+    const location = useLocation();
+      const navigate = useNavigate();
+      console.log(' login page', location);
+  
+      const handleLogin = e =>{
+        e.preventDefault();
+        const form =new FormData(e.currentTarget);
+      const email=form.get('email');
+      const password=form.get('password');
+    console.log(email,password);
+      signIn(email,password)
+     
+      .then(result=>{
+          console.log(result.user)
+          Swal.fire({
+           icon: 'success',
+           title: 'Login Succesful',
+           showConfirmButton: false,
+           timer: 1500
+         })
+            
+         // navigate after login
+         navigate(location?.state ? location.state : '/');
+      })
+      .catch(error=>{
+        console.error(error)
+        Swal.fire({
+         icon: 'error',
+         title: 'Oops...',
+         text: 'Something went wrong!',
+        
+       })
+      }
+      )
+}
     return (
         <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/XYRMxPN/12.jpg)'}}>
         <div className="">
@@ -11,8 +53,8 @@ const Login = () => {
 <div data-aos="fade-down"  data-aos-easing="ease-out-cubic"  data-aos-duration="2000" className="card mx-24 flex-shrink-0 w-full text-white   bg-transparent">
   <form  className="card-body  ">
   <h1 data-aos="fade-right"  data-aos-easing="ease-out-cubic"  data-aos-duration="2000" className="text-5xl text-white font-bold text-center">Login now!</h1>
-  {/* onSubmit={handleLogin} */}
-    <div className="form-control ">
+  
+    <div onSubmit={handleLogin} className="form-control ">
       <label className="label">
         <span className="label-text">Email</span>
       </label>
@@ -28,7 +70,7 @@ const Login = () => {
     <div className="form-control mt-6">
       <button className="btn bg-[#495737] text-white">Login</button>
 
-      {/* <div className='flex items-center justify-center my-2'><GoogleLogin ></GoogleLogin></div> */}
+      <div className='flex items-center justify-center my-2'><GoogleLogin ></GoogleLogin></div>
       
     </div>
 
