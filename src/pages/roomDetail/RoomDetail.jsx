c4053e4424b8f5eb2fbfe5b4cc54b7840e63c6b2
aubonee <chytahsin.2210@gmail.com>
2023-11-07 20:09:42 +0600

    room detail page modified

--- src/pages/roomDetail/RoomDetail.jsx
+++ src/pages/roomDetail/RoomDetail.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom/dist';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
 
 const RoomDetail = () => {
     const detail =useLoaderData();
-    const {_id, room_image, description, room_size}= detail;
+    const {_id,title,availability, room_images, description,price_per_night,quantity,special_offers, room_size}= detail;
+    const [selectedDate, setDate] =useState(null)
     const handlebook =() => {
     }
        // const newItem={...detail,email:user?.email}
@@ -28,37 +31,47 @@ const RoomDetail = () => {
     //     })
      
   
-     
+     const first_image = room_images[0];
+     const second_image = room_images[1];
       
     return (
-        <div>
+        <div className='flex items-center justify-center flex-col'>
             room detail
-            <div className='flex items-center justify-center'>
+            <div className='flex  justify-center flex-col my-3 bg-base-100 shadow-xl p-5 max-w-[920px] '>
            
 
-           <div className=" my-3 card lg:card-side w-[920px] bg-base-100 shadow-xl">
- <figure><img className='h-[580px] w-[720px] bg-gray-300' src={room_image} alt="Shoes" /></figure>
- <div className="card-body">
-   <h2 className="card-title">
-   {description}
-     {/* <div className="badge badge-secondary">{price} BDT</div> */}
-   </h2>
-  
-   <div>
-    {/* <div className="badge badge-outline  p-2 m-2">Brand:{brandname}</div> 
-     <div className="badge badge-outline p-2 m-2">Rating : {rating}</div>
-     <div className="badge badge-outline  p-2 m-2">Category :{category}</div>*/}
-    </div> 
-   <div className="card-actions justify-center">
-   
-      <br />
-      <div className='flex justify-end items-center'> 
-       <button className='btn bg-red-400' onClick={()=>handlebook()}>Book now</button>
-     </div>
-   
-   </div>
- </div>
+           <div className="flex gap-5">
+ <img className='max-w-[420px] bg-gray-300' src={first_image} alt="Shoes" />
+ <img className='max-w-[420px] bg-gray-300' src={second_image} alt="Shoes" />
+
+ 
+
+
 </div>
+<div className=" text-left mt-3">
+
+ 
+   <h2 className="font-semibold font-xl"> <span className='text-green-900 font-bold'>Room Name :</span> {title}</h2>
+   <h2 className="font-semibold font-xl"> <span className='text-green-900 font-bold'>Room Description :</span> {description}</h2>
+   <h2 className="font-semibold font-xl"> <span className='text-green-900 font-bold'>Quantity :</span> {quantity}</h2>
+   <h2 className="font-semibold font-xl"> <span className='text-green-900 font-bold'>Price per night :</span> {price_per_night}</h2>
+   <h2 className="font-semibold font-xl"> <span className='text-green-900 font-bold'>Room size :</span> {room_size}</h2>
+   <h2 className="font-semibold font-xl"> <span className='text-green-900 font-bold'>special_offers :</span> {special_offers}</h2>
+   <div className='availability'>
+
+   {quantity > 0 ? (
+  <h2 className="font-semibold font-xl"><span className='text-green-900 font-bold'>Status :</span> Available</h2>
+) : (
+  <h2 className="font-semibold font-xl"><span className='text-green-900 font-bold'>Status :</span> Unvailable</h2>
+)}
+   </div>
+   <div>
+    <div className="font-semibold font-xl"> <span className='text-green-900 font-bold'> Choose the Date:</span>  <DatePicker className='border-2 border-black' selected={selectedDate} onChange={date=>setDate(date)}></DatePicker></div>
+    <button className='btn bg-green-900 text-white my-3 p-3'> Book Now </button>
+   </div>
+  
+    
+    </div> 
        </div>
         </div>
     );
--- src/pages/rooms/RoomCard.jsx
+++ src/pages/rooms/RoomCard.jsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom/dist";
 
 
 const RoomCard = ({room}) => {
-    const {_id, room_image, description, room_size}= room;
+    const {_id, room_images, description, room_size}= room;
+   
+    const first_image = room_images[1];
 
     return (
      
@@ -10,7 +12,7 @@ const RoomCard = ({room}) => {
         
        <Link to={`/rooms/${_id}`}>
         <div  className="">
-        <figure  className="px-10 pt-10"><img src={room_image} alt="Shoes" className="rounded-none" /></figure>
+        <figure  className="px-10 pt-10"><img src={first_image} alt="Shoes" className="rounded-none" /></figure>
         {/* <div className="card-body">
           <h2 className="card-title">{description}</h2>
           <p>Room Size:{room_size}</p>
