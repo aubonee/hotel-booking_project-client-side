883e38cdcf3cccf1ee3f5cff2bb01b8b8873b5b6
aubonee <chytahsin.2210@gmail.com>
2023-11-09 19:28:32 +0600

    romms page styling modified

--- src/pages/rooms/Rooms.jsx
+++ src/pages/rooms/Rooms.jsx
@@ -12,7 +12,7 @@ const Rooms = () => {
     },[])
     return (
         <div className=" w-11/12 mx-auto ">
-            <h3 className="text-center text-xl">rooms</h3>
+            <h3 className=" font-extrabold text-center text-[#495737] text-5xl"> All Rooms</h3>
        
         <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               
